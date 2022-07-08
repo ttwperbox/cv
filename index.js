07938a4055492b2460d6a2ea6cba23dbe6862e13
Thirty Thousand Won per Box <ttwperbox@gmail.com>
2022-07-08 17:23:49 +0900

    make cv

--- index.js
+++
@@ -1,8 +0,0 @@
-function printExpression(value) {
-    document.querySelector("#expression").innerHTML += value;
-  }
-  
-  function calculate() {
-    let expression = document.querySelector("#expression").innerHTML;
-    document.querySelector("#answer").innerHTML = eval(expression);
-  }
